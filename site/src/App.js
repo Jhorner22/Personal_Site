fa54d8dff8138ebaeb52fd4827261a2f2a61b800
jphorner22 <jphorne22@gmail.com>
2021-04-26 07:15:02 -0500

    GROUNDWORK DONE

---
+++ site/src/App.js
@@ -0,0 +1,26 @@
+import React, { useState, useEffect } from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
+
+import './App.scss';
+
+import {Test} from './pages/test/component/Test';
+
+function App() {
+    //this is the App^TM
+    return (
+        <Router>
+            <Switch>
+                {/* all the different routes go here aka the pages with the website.com/"ROUTE" */}
+                <Route path="/test">
+                    <test/>
+                </Route>
+            </Switch>
+        </Router>
+    )
+}
+
+export default App;
---
+++ site/src/pages/test/component/Test.jsx
@@ -0,0 +1,2 @@
+import React, { useState } from 'react';
+import { Redirect, useLocation } from 'react-router';
